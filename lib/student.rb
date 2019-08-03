180bfd242ea95cf45381ffe8f60a2bdb7f77f993
Jane Justiz <janejustiz@gmail.com>
2019-08-03 02:34:49 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -21,21 +21,36 @@ class Student
   end
 
   def self.drop_table
+    sql <<-SQL
+    DROP TABLE students
+    SQL
+    
+    DB[:conn].execute(sql)
   end
 
   def save
+    sql <<-SQL
+    SQL
   end
 
   def self.create
+    sql <<-SQL
+    SQL
   end
 
   def self.new_from_db
+    sql <<-SQL
+    SQL
   end
 
   def self.find_by_name
+    sql <<-SQL
+    SQL
   end
 
   def update
+    sql <<-SQL
+    SQL
   end
 
 end
