d691381e393dbd45e2534a370333814711ec87de
Jane Justiz <janejustiz@gmail.com>
2019-08-03 02:31:23 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,11 +3,31 @@ require_relative "../config/environment.rb"
 class Student
   attr_accessor :name, :grade
   attr_reader :id
-  
+
   def initialize(name, grade, id=nil)
     @name=name
     @grade=grade
   end
 
+  def self.create_table
+  end
+
+  def self.drop_table
+  end
+
+  def save
+  end
+
+  def self.create
+  end
+
+  def self.new_from_db
+  end
+
+  def self.find_by_name
+  end
+
+  def update
+  end
 
 end
